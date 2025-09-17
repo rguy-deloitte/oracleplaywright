20faedb705a72f26be991240d3c9363b5a76ec61
Tarun Dhillon <tdhillon@deloitte.co.uk>
2025-09-17 10:05:25 +0100

    Remove unused files and clean up project structure

--- tests/accounts payable/supplier.spec.ts
+++ tests/accounts payable/supplier.spec.ts
@@ -5,35 +5,25 @@ import * as csTest from './create-supplier'
 test.slow();
 let supplierItems: any[] = [];
 
-// test.beforeAll(async ({ page }) => {
-//     // const context = await browser.newContext({
-//     //     // Comment out or remove storageState if you don't have .auth/auth-state.json
-//     //     storageState: '../auth/auth-state.json'
-//     // });
-//     // const page = await context.newPage();
+test('Create Suppliers', async ({ page }, testInfo) => {
+    console.log('Generating supplier data...');
+});
+
+// test.describe('Supplier Tests', async () => {
+    
 //     console.log('Generating supplier data...');
 //     supplierItems = await csTest.fakeData(5);
 //     console.log(supplierItems?.length + ' suppliers generated ');
-
-//     await fire();
-// });
-
-
-test.describe('Supplier Tests', async () => {
-    
-    console.log('Generating supplier data...');
-    supplierItems = await csTest.fakeData(5);
-    console.log(supplierItems?.length + ' suppliers generated ');
-    let index = 0;
-    // for (let index = 0; index < supplierItems.length; index++) { 
-    for await (const supplierItem of supplierItems) {
-        test(`Process Supplier: ${supplierItem.Supplier}`, async ({ page }, testInfo) => {
-            console.log(`Running test for supplier: ${supplierItem.Supplier}`);
-            await csTest.createSupplier(page, testInfo, supplierItem, index);
-        });
-        index++
-    }
-}); 
+//     let index = 0;
+//     for await (const supplierItem of supplierItems) {
+//         console.log(`Processing supplier: ${supplierItem.Supplier}`);
+//         // await test(`Process Supplier: ${supplierItem.Supplier}`, async ({ page }, testInfo) => {
+//         //     console.log(`Running test for supplier: ${supplierItem.Supplier}`);
+//         //     await csTest.createSupplier(page, testInfo, supplierItem, index);
+//         // });
+//         index++; 
+//     }
+// })
 
 
 type SupplierItem = {
--- tests/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.getByRole('link', { name: 'Navigator' }).click();
-  await page.locator('div').filter({ hasText: 'Procurement' }).click();
-  await page.getByRole('link', { name: 'Suppliers' }).click();
-  await page.getByRole('link', { name: 'Tasks' }).click();
-  await page.getByRole('link', { name: 'Create Supplier' }).click();
-});
