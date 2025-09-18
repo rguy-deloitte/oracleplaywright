307b9323f569bdfdf70552ba234e9d12f68101ea
Luke Chaplin <lchaplin@deloitte.co.uk>
2025-09-18 12:28:20 +0100

    Returned some comments in case they are needed in future

--- tests/misc/GL-create-manual-journal.spec.ts
+++ tests/misc/GL-create-manual-journal.spec.ts
@@ -57,3 +57,75 @@ test.describe('Post Manual Journal', () => {
     await expect(journalLinesTableRows).toHaveCount(loopData.length - 1);
   });
 });
+
+// await journalLinesTableRows.nth(0).getByRole('textbox', { name: 'Account' }).fill(jlAccount);
+    // await journalLinesTableRows.nth(0).getByRole('textbox', { name: 'Entered Debit' }).fill('11.11');
+    // await journalLinesTableRows.nth(0).getByRole('textbox', { name: 'Description Mandatory' }).fill('abc');
+    // await journalLinesTableRows.nth(0).getByRole('textbox', { name: 'Description Mandatory' }).press('Enter');
+
+    // await journalLinesTableRows.nth(1).getByRole('textbox', { name: 'Account' }).fill(jlAccount);
+    // await journalLinesTableRows.nth(1).getByRole('textbox', { name: 'Entered Debit' }).fill('22.22');
+    // await journalLinesTableRows.nth(1).getByRole('textbox', { name: 'Description Mandatory' }).fill('def');
+    // await journalLinesTableRows.nth(1).getByRole('textbox', { name: 'Description Mandatory' }).press('Enter');
+
+    // await page.getByRole('button', { name: 'Add Row', exact: true }).click();
+
+    // await journalLinesTableRows.nth(2).getByRole('textbox', { name: 'Account' }).fill(jlAccount);
+    // await journalLinesTableRows.nth(2).getByRole('textbox', { name: 'Entered Debit' }).fill('33.33');
+    // await journalLinesTableRows.nth(2).getByRole('textbox', { name: 'Description Mandatory' }).fill('ghi');
+    // await journalLinesTableRows.nth(2).getByRole('textbox', { name: 'Description Mandatory' }).press('Enter');
+
+    //  //populate journal  
+    // await test.step('Populate Journal', async () => {
+    //     await page.getByRole('textbox', { name: 'Journal Batch' }).fill('Test Journal Batch');
+    //     await page.getByRole('textbox', { name: 'Accounting Period' }).fill('Apr-26');
+    //     await page.getByRole('textbox', { name: 'Journal', exact: true }).fill('Test Journal');
+    //     await page.getByRole('textbox', { name: 'Category' }).fill('Work In Progress');
+    //     await testInfo.attach('Populate Journal Information', { body: await page.screenshot(), contentType: 'image/png' });
+        
+    //     //select account for journal line 1
+
+    //     await page.getByRole('textbox', { name: 'Journal', exact: true }).click();
+    //     await page.getByRole('textbox', { name: 'Journal', exact: true }).fill('Test Journal');
+    //     await testInfo.attach('Populate Journal Batch', { body: await page.screenshot(), contentType: 'image/png' });
+
+    //     //await page.getByRole('combobox', { name: 'Accounting Period' }).locator('a').click();
+    //     await page.getByRole('combobox', { name: 'Accounting Period' }).click();
+    //     await page.getByRole('gridcell', { name: 'Feb-24' }).click();
+        
+
+    //     //await page.getByRole('combobox', { name: 'Accounting Period' }).click();
+    //     //await page.getByRole('combobox', { name: 'Accounting Period' }).fill('Feb-24');
+    //     //await page.getByRole('combobox', { name: 'Accounting Period' }).press('Enter');
+
+    //     await testInfo.attach('Input accounting Period', { body: await page.screenshot(), contentType: 'image/png' });
+        
+    //     //Select Account
+    //     await page.getByLabel('Select Journal').click();
+    //     await page.getByRole('link', { name: 'Select: Account' }).click();
+    //     await page.getByRole('button', { name: 'Search' }).click();
+    //     await page.getByRole('cell', { name: '999999' }).first().click();
+    //     await page.getByRole('button', { name: 'OK' }).click();
+    //     await testInfo.attach('Select Journal Line Account', { body: await page.screenshot(), contentType: 'image/png' });
+
+    //     //Populate Journal Lines 
+    //     await page.getByRole('textbox', { name: 'Entered Debit' }).click();
+    //     await page.getByRole('textbox', { name: 'Entered Debit' }).fill('10000');
+    //     await page.getByRole('textbox', { name: 'Description Mandatory' }).click();
+    //     await page.getByRole('textbox', { name: 'Description Mandatory' }).fill('Test');
+    //     await testInfo.attach('Populate Journal Line', { body: await page.screenshot(), contentType: 'image/png' });
+
+    //     await page.getByRole('link', { name: 'Select: Account' }).click();
+    //     await page.getByRole('button', { name: 'Search' }).click();
+    //     await page.locator('[id="__af_Z_window"]').getByText('8790000000').click();
+    //     await page.getByRole('button', { name: 'OK' }).click();
+    //     await page.getByLabel('Select Journal').click();
+    //     await page.getByRole('button', { name: 'OK' }).click();
+    //     await page.getByRole('textbox', { name: 'Entered Credit' }).click();
+    //     await page.getByRole('textbox', { name: 'Entered Credit' }).fill('10000');
+
+    //     await testInfo.attach('Journal Saved', { body: await page.screenshot(), contentType: 'image/png' });
+    //     await page.getByRole('link', { name: 'Post', exact: true }).click();
+    //     await testInfo.attach('Journal Posted', { body: await page.screenshot(), contentType: 'image/png' });
+
+    // });
