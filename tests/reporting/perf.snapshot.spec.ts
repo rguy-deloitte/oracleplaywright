a12077a8dff9bd5c87b4b0691671c4e6cd933093
Richard Guy <richardguy@deloitte.co.uk>
2025-10-09 16:04:41 +0100

    Fixed report timings

--- tests/reporting/perf.snapshot.spec.ts
+++ tests/reporting/perf.snapshot.spec.ts
@@ -1,4 +1,4 @@
-// Run using: npx playwright test tests/reports/perf.snapshot.spec.ts --ui
+// Run using: npx playwright test tests/reporting/perf.snapshot.spec.ts --ui
 import { test, expect } from '@playwright/test';
 import * as reportService from './report';
 import { navigateToUrl } from '../util/navigation'
@@ -24,6 +24,7 @@ test.describe('Report performance', async () => {
         let testArray: any[][] = xlsx.utils.sheet_to_json(testBook.Sheets["Sheet1"], { raw: false, header: 1 });
 
         let resultArray = testArray;
+        let startTime: number, endTime :number;
 
         for (let i = 1; i < testArray.length; i++) {
             let resultRow = testArray[i];
@@ -33,27 +34,29 @@ test.describe('Report performance', async () => {
             await test.step(`Running ${reportName}`, async () => {
                 const downloadPromise = page.waitForEvent('download');
                 await navigateToUrl(page, url, testInfo, 'Navigate to report url');
-                let siteInitialised = Date.now();
                 let runTime;
                 let download;
 
                 // Check if automatically downloading xlsx file or if need to manually request
                 let fileIcon = await page.locator("[id='xdo:viewFormatIcon']").getAttribute("src");
+                startTime = Date.now();
 
                 if (fileIcon != null && /.*preview_excel.*/.test(fileIcon)) {
-                    download = await downloadPromise;
-                    runTime = Date.now() - siteInitialised;
-                    console.log(reportName + " downloaded immediately");
+                    endTime = Date.now();
+                    runTime = endTime - startTime;
+                    console.log(reportName + " downloaded immediately", new Date(startTime).toLocaleTimeString(), new Date(endTime).toLocaleTimeString(), (runTime / 1000).toFixed(2));
                 } else {
                     let dropdownButton = page.locator("[src='/xmlpserver/static/v20250625.0650/theme/alta/images/toolbar/dropdown_md.png']")
                     await dropdownButton.click();
-                    let startTime = Date.now();
+                    startTime = Date.now();
                     await page.getByRole('link', { name: 'Excel (*.xlsx)' }).click();
                     download = await downloadPromise;
-                    runTime = Date.now() - startTime;
-                    console.log(reportName + " downloaded after click through");
+                    endTime = Date.now();
+                    runTime = endTime - startTime;
+                    console.log(reportName + " downloaded after click through", new Date(startTime).toLocaleTimeString(), new Date(endTime).toLocaleTimeString(), (runTime / 1000).toFixed(2));
                 }
 
+                // console.log(download);
                 let downloadPath = __dirname + '/../../excel-data-files/tmp/' + download.suggestedFilename()
                 await download.saveAs(downloadPath);
 
@@ -61,7 +64,9 @@ test.describe('Report performance', async () => {
                 let reportRows = xlsx.utils.sheet_to_json(xlsx.readFile(downloadPath).Sheets["Sheet1"], { raw: false, header: 1 }).length - 1;
 
                 resultRow[3] = reportRows;
-                resultRow[6] = runTime/60;
+                resultRow[4] = new Date(startTime).toLocaleTimeString();
+                resultRow[5] = new Date(endTime).toLocaleTimeString();
+                resultRow[6] = runTime/1000;
 
                 resultArray[i] = resultRow;
                 let resultSheet = xlsx.utils.aoa_to_sheet(resultArray);
